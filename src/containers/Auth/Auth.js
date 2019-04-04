19c8e9241252337466b28ffe89cae5d22b99e425
Vitalii Hudanets <wburn@hostivar-0217.koleje.cuni.cz>
2019-04-04 15:42:33 +0200

    Auth dummy container

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class Auth extends Component {
     state = {
@@ -21,15 +21,52 @@ class Auth extends Component {
         return (
             <div className='Auth'>
                 <form onSubmit={this.submitForm}>
-                    <div className='Auth__input'>
-                        <label htmlFor='email'>email</label>
-                        <input id='email' name='email' type='email' value={this.state.email} onChange={this.handleSubmit} />
+                    <div>
+                        <input
+                            id='email'
+                            name='email'
+                            type='email'
+                            value={this.state.email}
+                            onChange={this.handleSubmit}
+                            required />
+                        {
+                            this.state.email.length === 0 ?
+                                <Fragment>
+                                    <label
+                                        htmlFor='email'>email</label>
+                                </Fragment> :
+                                <Fragment>
+                                    <label
+                                        style={{top: '-.8rem', fontSize: '1.2rem'}}
+                                        htmlFor='email'>email</label>
+                                </Fragment>
+                        }
+                        <span />
                     </div>
-                    <div className='Auth__input'>
-                        <label htmlFor='password'>heslo</label>
-                        <input id='password' name='password' type='password' value={this.state.password} onChange={this.handleSubmit} />
+
+                    <div>
+                        <input
+                            id='password'
+                            name='password'
+                            type='password'
+                            value={this.state.password}
+                            onChange={this.handleSubmit}
+                            required />
+                        {
+                            this.state.password.length === 0 ?
+                                <Fragment>
+                                    <label
+                                        htmlFor='password'>heslo</label>
+                                </Fragment> :
+                                <Fragment>
+                                    <label
+                                        style={{top: '-.5rem', fontSize: '1.2rem'}}
+                                        htmlFor='password'>heslo</label>
+                                </Fragment>
+                        }
+                        <span />
                     </div>
-                    <button>log in</button>
+                    <button>přihlásit se</button>
                 </form>
             </div>
         )
